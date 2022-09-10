470837fa20bcb3d2df9f9ca9a9a1c44741dcbf39
yazz <joo21safe@gmail.com>
2022-09-10 17:00:55 +0300

    trrrwewsdsdf

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 
 const app = express();
 
+const port = process.env.PORT || 42000;
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hrsystem'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/hrsystem/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
