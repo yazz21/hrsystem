58c652ffd2df3a4e719edcb16f71e1e27693166e
admin <codendi-admin@tuleap.alm.com>
2022-09-23 16:46:22 +0300

    s

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the port
 app.listen(port,"0.0.0.0");
